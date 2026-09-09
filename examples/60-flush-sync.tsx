66ae32ecc63aa76d2cdacdfcf48ca3e8311ba313
Nathanne Isip <nathanne@nthnn.dev>
2026-09-09 19:09:45 +0800

    feat: add flushSync example

---
+++ examples/60-flush-sync.tsx
@@ -0,0 +1,41 @@
+/*
+ * Helvetium DOM - Helvetium Framework, A Newfoundcodes project.
+ *
+ * Copyright (C) 2026 Jonathan Eldy Baldivicio
+ *
+ * Author: Jonathan Eldy Baldivicio
+ * Contact: jonathaneldy.baldivicio@newfoundcodes.com
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { h, createRoot, flushSync, useState, type StateSetter } from '@newfoundcodes/helvetium-dom';
+
+let setOpen: StateSetter<boolean>;
+
+function App() {
+  const [open, set] = useState(false);
+  setOpen = set;
+  return <div data-open={open}>{open ? 'open' : 'closed'}</div>;
+}
+
+export function mountAndOpen(container: HTMLElement) {
+  const root = createRoot(container);
+  root.render(<App />);
+
+  flushSync(() => setOpen(true));
+  return root;
+}

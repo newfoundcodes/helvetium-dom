01a78b83e9cb95bbde462048c67d0faf38490272
Nathanne Isip <nathanne@nthnn.dev>
2026-09-09 19:09:43 +0800

    feat: add example document shell

---
+++ examples/document-shell.tsx
@@ -0,0 +1,45 @@
+/*
+ * Helvetium DOM - Helvetium Framework, A Newfoundcodes project.
+ *
+ * Copyright (C) 2026 Jonathan Eldy Baldivicio
+ *
+ * Author: Jonathan Eldy Baldivicio
+ * Contact: jonathaneldy.baldivicio@newfoundcodes.com
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { Fragment, h, type VNodeChild } from '@newfoundcodes/helvetium-dom';
+
+function DocumentShell({ children }: { children?: VNodeChild }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <title>Helvetium document shell</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
+
+export const documentShell = (
+  <DocumentShell>
+    <main>Hello</main>
+  </DocumentShell>
+);

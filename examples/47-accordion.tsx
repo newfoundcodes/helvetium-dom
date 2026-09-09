7f889a233df917ca023fe6f05b50ea508d236b4d
Nathanne Isip <nathanne@nthnn.dev>
2026-09-09 19:09:44 +0800

    feat: add accordion example

---
+++ examples/47-accordion.tsx
@@ -0,0 +1,44 @@
+/*
+ * Helvetium DOM - Helvetium Framework, A Newfoundcodes project.
+ *
+ * Copyright (C) 2026 Jonathan Eldy Baldivicio
+ *
+ * Author: Jonathan Eldy Baldivicio
+ * Contact: jonathaneldy.baldivicio@newfoundcodes.com
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { h, createRoot, useState, useId } from '@newfoundcodes/helvetium-dom';
+
+function Accordion() {
+  const [open, setOpen] = useState(false);
+  const id = useId();
+  return (
+    <section>
+      <button aria-expanded={open} aria-controls={id} onClick={() => setOpen((v) => !v)}>
+        Details
+      </button>
+      {open ? <div id={id}>Expanded content</div> : null}
+    </section>
+  );
+}
+
+export function mount(container: HTMLElement) {
+  const root = createRoot(container);
+  root.render(<Accordion />);
+  return root;
+}

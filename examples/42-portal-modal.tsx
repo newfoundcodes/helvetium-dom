510a6a9b251d7c7e8bf2b0ef1ec42fa184fc0168
Nathanne Isip <nathanne@nthnn.dev>
2026-09-09 19:09:44 +0800

    feat: add portal modal example

---
+++ examples/42-portal-modal.tsx
@@ -0,0 +1,41 @@
+/*
+ * Helvetium DOM - Helvetium Framework, A Newfoundcodes project.
+ *
+ * Copyright (C) 2026 Jonathan Eldy Baldivicio
+ *
+ * Author: Jonathan Eldy Baldivicio
+ * Contact: jonathaneldy.baldivicio@newfoundcodes.com
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { h, createPortal, createRoot } from '@newfoundcodes/helvetium-dom';
+
+export function mount(container: HTMLElement, overlay: HTMLElement) {
+  const root = createRoot(container);
+  root.render(
+    <main>
+      Application
+      {createPortal(
+        <div role="dialog" aria-modal="true">
+          Portal dialog
+        </div>,
+        overlay,
+      )}
+    </main>,
+  );
+  return root;
+}

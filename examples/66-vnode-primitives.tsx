19f1f797a50798a7ba5a6d762b6bbf1b03877ffa
Nathanne Isip <nathanne@nthnn.dev>
2026-09-09 19:09:45 +0800

    feat: add VNode primitives example

---
+++ examples/66-vnode-primitives.tsx
@@ -0,0 +1,45 @@
+/*
+ * Helvetium DOM - Helvetium Framework, A Newfoundcodes project.
+ *
+ * Copyright (C) 2026 Jonathan Eldy Baldivicio
+ *
+ * Author: Jonathan Eldy Baldivicio
+ * Contact: jonathaneldy.baldivicio@newfoundcodes.com
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import {
+  createRoot,
+  createTextVNode,
+  createVNode,
+  h,
+  isVNode,
+  normalizeChildren,
+} from '@newfoundcodes/helvetium-dom';
+
+export function mount(container: HTMLElement) {
+  const children = normalizeChildren(['A', 1, createTextVNode('B')]);
+  const vnode = createVNode('div', { id: 'low-level' }, children);
+
+  if (!isVNode(vnode)) {
+    throw new Error('Expected VNode');
+  }
+
+  const root = createRoot(container);
+  root.render(vnode);
+  return root;
+}

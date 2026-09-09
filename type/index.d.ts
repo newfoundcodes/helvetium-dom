6ab6382b53f7e0c4d262d257ad3a1ea66f13713a
Nathanne Isip <nathanne@nthnn.dev>
2026-09-09 19:09:45 +0800

    feat: add public type declarations

---
+++ type/index.d.ts
@@ -0,0 +1,71 @@
+/*
+ * Helvetium DOM - Helvetium Framework, A Newfoundcodes project.
+ *
+ * Copyright (C) 2026 Jonathan Eldy Baldivicio
+ *
+ * Author: Jonathan Eldy Baldivicio
+ * Contact: jonathaneldy.baldivicio@newfoundcodes.com
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+export type {
+  ComponentType,
+  FunctionComponent,
+  Key,
+  PrimitiveChild,
+  Ref,
+  RefCallback,
+  RefObject,
+  Root,
+  RootOptions,
+  VNode,
+  VNodeChild,
+  VNodeKind,
+} from './core.js';
+
+export type {
+  CSSProperties,
+  DOMAttributes,
+  EventHandler,
+  HTMLAttributes,
+  SVGAttributes,
+  StyleValue,
+} from './dom.js';
+
+export type { Context } from './context.js';
+export type { StateSetter, StateUpdater } from './hooks.js';
+export type { RenderToStringOptions } from './server.js';
+
+export type {
+  AnchorProps,
+  BrowserRouter,
+  StaticRouter,
+  Router,
+  CreateBrowserRouterOptions,
+  CreateStaticRouterOptions,
+  CreateRouterCommonOptions,
+  MatchRoutesOptions,
+  NavigateOptions,
+  RouteComponentProps,
+  RouteDefinition,
+  RouteMetadata,
+  RouteMatch,
+  RouterLocation,
+  RouterOutletProps,
+  RouterProviderProps,
+  RouterTarget,
+} from './router.js';

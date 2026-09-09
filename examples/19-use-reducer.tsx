4d0b0c28c23279527db696719141581526498469
Nathanne Isip <nathanne@nthnn.dev>
2026-09-09 19:09:44 +0800

    feat: add useReducer example

---
+++ examples/19-use-reducer.tsx
@@ -0,0 +1,48 @@
+/*
+ * Helvetium DOM - Helvetium Framework, A Newfoundcodes project.
+ *
+ * Copyright (C) 2026 Jonathan Eldy Baldivicio
+ *
+ * Author: Jonathan Eldy Baldivicio
+ * Contact: jonathaneldy.baldivicio@newfoundcodes.com
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { h, createRoot, useReducer } from '@newfoundcodes/helvetium-dom';
+
+type Action = { type: 'inc' } | { type: 'dec' };
+
+function reducer(state: number, action: Action) {
+  return action.type === 'inc' ? state + 1 : state - 1;
+}
+
+function App() {
+  const [value, dispatch] = useReducer(reducer, 0);
+  return (
+    <div>
+      <button onClick={() => dispatch({ type: 'dec' })}>-</button>
+      <output>{value}</output>
+      <button onClick={() => dispatch({ type: 'inc' })}>+</button>
+    </div>
+  );
+}
+
+export function mount(container: HTMLElement) {
+  const root = createRoot(container);
+  root.render(<App />);
+  return root;
+}

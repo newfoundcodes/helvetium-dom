db7e85c9b96840b7ee78ba65d12a00b765ecd1b5
Nathanne Isip <nathanne@nthnn.dev>
2026-09-09 19:09:44 +0800

    feat: add recursive component example

---
+++ examples/16-recursive-component.tsx
@@ -0,0 +1,52 @@
+/*
+ * Helvetium DOM - Helvetium Framework, A Newfoundcodes project.
+ *
+ * Copyright (C) 2026 Jonathan Eldy Baldivicio
+ *
+ * Author: Jonathan Eldy Baldivicio
+ * Contact: jonathaneldy.baldivicio@newfoundcodes.com
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { h, createRoot } from '@newfoundcodes/helvetium-dom';
+
+type NodeModel = { label: string; children?: NodeModel[] };
+
+function Tree({ node }: { node: NodeModel }) {
+  return (
+    <li>
+      {node.label}
+      {node.children?.length ? (
+        <ul>
+          {node.children.map((child) => (
+            <Tree key={child.label} node={child} />
+          ))}
+        </ul>
+      ) : null}
+    </li>
+  );
+}
+
+export function mount(container: HTMLElement) {
+  const root = createRoot(container);
+  root.render(
+    <ul>
+      <Tree node={{ label: 'src', children: [{ label: 'index.ts' }] }} />
+    </ul>,
+  );
+  return root;
+}

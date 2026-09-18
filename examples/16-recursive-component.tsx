b0253e0ceab32fe1b716783d24cbe9a540c5bf86
Nathanne Isip <nathanne@nthnn.dev>
2026-09-19 00:36:52 +0800

    refactor: update examples/16-recursive-component.tsx for react-jsx transform

--- examples/16-recursive-component.tsx
+++ examples/16-recursive-component.tsx
@@ -22,7 +22,7 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { h, createRoot } from '@newfoundcodes/helvetium-dom';
+import { createRoot } from '@newfoundcodes/helvetium-dom';
 
 type NodeModel = { label: string; children?: NodeModel[] };
 
@@ -45,7 +45,7 @@ export function mount(container: HTMLElement) {
   const root = createRoot(container);
   root.render(
     <ul>
-      <Tree node={{ label: 'src', children: [{ label: 'index.ts' }] }} />
+      <Tree node={{ label: 'src', children: [{ label: 'index.ts' }, { label: 'global.css' }] }} />
     </ul>,
   );
   return root;

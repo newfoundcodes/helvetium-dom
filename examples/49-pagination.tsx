ee8e001d230658752416bfca44b0d84425ba0cec
Nathanne Isip <nathanne@nthnn.dev>
2026-09-19 00:36:53 +0800

    refactor: update examples/49-pagination.tsx for react-jsx transform

--- examples/49-pagination.tsx
+++ examples/49-pagination.tsx
@@ -22,7 +22,7 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { h, createRoot, useState } from '@newfoundcodes/helvetium-dom';
+import { createRoot, useState } from '@newfoundcodes/helvetium-dom';
 
 function Pager() {
   const [page, setPage] = useState(1);

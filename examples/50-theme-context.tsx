8db69810882f474877d75ba5e3afd7969bf30528
Nathanne Isip <nathanne@nthnn.dev>
2026-09-19 00:36:53 +0800

    refactor: update examples/50-theme-context.tsx for react-jsx transform

--- examples/50-theme-context.tsx
+++ examples/50-theme-context.tsx
@@ -22,7 +22,7 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { h, createContext, createRoot, useContext, useState } from '@newfoundcodes/helvetium-dom';
+import { createContext, createRoot, useContext, useState } from '@newfoundcodes/helvetium-dom';
 
 const Theme = createContext<'light' | 'dark'>('light');
 

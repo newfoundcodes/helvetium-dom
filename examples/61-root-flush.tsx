023d9e8445c4e13ed2233fab7f6c9534b22eeac3
Nathanne Isip <nathanne@nthnn.dev>
2026-09-19 00:36:53 +0800

    refactor: update examples/61-root-flush.tsx for react-jsx transform

--- examples/61-root-flush.tsx
+++ examples/61-root-flush.tsx
@@ -22,7 +22,7 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { h, createRoot, useState, type StateSetter } from '@newfoundcodes/helvetium-dom';
+import { createRoot, useState, type StateSetter } from '@newfoundcodes/helvetium-dom';
 
 let update: StateSetter<number>;
 

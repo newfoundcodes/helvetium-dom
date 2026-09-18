8225120dc29dda79db2430520db4aa0cfe4ee31e
Nathanne Isip <nathanne@nthnn.dev>
2026-09-19 00:36:52 +0800

    refactor: update examples/12-memo-component.tsx for react-jsx transform

--- examples/12-memo-component.tsx
+++ examples/12-memo-component.tsx
@@ -22,7 +22,7 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { h, createRoot, memo } from '@newfoundcodes/helvetium-dom';
+import { createRoot, memo } from '@newfoundcodes/helvetium-dom';
 
 const Label = memo(function Label({ value }: { value: string }) {
   return <span>{value}</span>;

3a1874ed6bcfcf0e2e74a74fc1793c3668c3a2de
Nathanne Isip <nathanne@nthnn.dev>
2026-09-19 00:36:52 +0800

    refactor: update examples/11-component-composition.tsx for react-jsx transform

--- examples/11-component-composition.tsx
+++ examples/11-component-composition.tsx
@@ -22,7 +22,7 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { Fragment, h, createRoot } from '@newfoundcodes/helvetium-dom';
+import { createRoot } from '@newfoundcodes/helvetium-dom';
 
 const Header = () => (
   <header>

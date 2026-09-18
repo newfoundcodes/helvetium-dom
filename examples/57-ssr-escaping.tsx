794f17cbda18a207f6f96a4e13bdffdb92abbc19
Nathanne Isip <nathanne@nthnn.dev>
2026-09-19 00:36:53 +0800

    refactor: update examples/57-ssr-escaping.tsx for react-jsx transform

--- examples/57-ssr-escaping.tsx
+++ examples/57-ssr-escaping.tsx
@@ -22,7 +22,6 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { h } from '@newfoundcodes/helvetium-dom';
 import { renderToString } from '@newfoundcodes/helvetium-dom/server';
 
 export const html = renderToString(<p title={'"quoted"'}>{'<script>alert(1)</script>'}</p>);

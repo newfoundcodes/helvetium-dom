363cf250690620234f7b53be29dfc6a6fbb218b7
Nathanne Isip <nathanne@nthnn.dev>
2026-09-19 00:36:52 +0800

    refactor: update examples/35-select.tsx for react-jsx transform

--- examples/35-select.tsx
+++ examples/35-select.tsx
@@ -22,7 +22,7 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { Fragment, h, createRoot, useState } from '@newfoundcodes/helvetium-dom';
+import { createRoot, useState } from '@newfoundcodes/helvetium-dom';
 
 function App() {
   const [role, setRole] = useState('developer');
